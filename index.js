667000fe1f929152fbce72587d9ed7694e8f20b4
Lamasu <rafaerthal@gmail.com>
2014-08-26 20:57:04 -0300

    getting all pages and using default json parser

--- index.js
+++ index.js
@@ -9,230 +9,132 @@ exports.configUri = function (newUri) {
   uri = newUri;
 };
 
-exports.courses = function (page, next) {
+function downloadPages(url, next) {
   'use strict';
 
-  return httpRequest(uri + '/courses?page=' + page, function (error, res, body) {
-    var courses;
-    if (error) {
-      error = new VError(error, 'error requesting courses');
-      return next(error);
-    }
-    try {
-      courses = JSON.parse(body);
-      return next(null, courses);
-    } catch (error) {
-      error = new VError(error, 'error parsing courses to json');
-      return next(error);
-    }
+  var lastPage, results, lastLength;
+  results = [];
+  lastLength = 0;
+  lastPage = 0;
+  async.doWhilst(function (next) {
+    return httpRequest({
+      'url'  : uri + url,
+      'json' : true,
+      'qs'   : {'page' : lastPage}
+    }, function (error, res, body) {
+      if (error) {
+        return next(error);
+      }
+      results = results.concat(body);
+      lastLength = body.length;
+      lastPage++;
+      return next();
+    });
+  }, function () {
+    return lastLength > 0;
+  }, function (error) {
+    return next(error, results);
   });
+}
+
+exports.courses = function (next) {
+  'use strict';
+
+  return downloadPages('/courses', next);
 };
 
 exports.course = function (id, next) {
   'use strict';
 
-  return httpRequest(uri + '/courses/' + id, function (error, res, body) {
-    var course;
-    if (error) {
-      error = new VError(error, 'error requesting course "$s"', id);
-      return next(error);
-    }
-    try {
-      course = JSON.parse(body);
-      return next(null, course);
-    } catch (error) {
-      error = new VError(error, 'error parsing course "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/courses/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
 
-exports.disciplines = function (page, next) {
+exports.disciplines = function (next) {
   'use strict';
 
-  return httpRequest(uri + '/disciplines?page=' + page, function (error, res, body) {
-    var disciplines;
-    if (error) {
-      error = new VError(error, 'error requesting disciplines');
-      return next(error);
-    }
-    try {
-      disciplines = JSON.parse(body);
-      return next(null, disciplines);
-    } catch (error) {
-      error = new VError(error, 'error parsing disciplines to json');
-      return next(error);
-    }
-  });
+  return downloadPages('/disciplines', next);
 };
 
 exports.discipline = function (id, next) {
   'use strict';
 
-  return httpRequest(uri + '/disciplines/' + id, function (error, res, body) {
-    var discipline;
-    if (error) {
-      error = new VError(error, 'error requesting discipline "$s"', id);
-      return next(error);
-    }
-    try {
-      discipline = JSON.parse(body);
-      return next(null, discipline);
-    } catch (error) {
-      error = new VError(error, 'error parsing discipline "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/disciplines/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
 
-exports.offerings = function (discipline, page, next) {
+exports.offerings = function (discipline, next) {
   'use strict';
 
-  return httpRequest(uri + '/disciplines/' + discipline + '/offerings?page=' + page, function (error, res, body) {
-    var offerings;
-    if (error) {
-      error = new VError(error, 'error requesting offerings');
-      return next(error);
-    }
-    try {
-      offerings = JSON.parse(body);
-      return next(null, offerings);
-    } catch (error) {
-      error = new VError(error, 'error parsing offerings to json');
-      return next(error);
-    }
-  });
+  return downloadPages('/disciplines/' + discipline + '/offerings', next);
 };
 
 exports.offering = function (discipline, id, next) {
   'use strict';
 
-  return httpRequest(uri + '/disciplines/' + discipline + '/offerings/' + id, function (error, res, body) {
-    var offering;
-    if (error) {
-      error = new VError(error, 'error requesting offering "$s"', id);
-      return next(error);
-    }
-    try {
-      offering = JSON.parse(body);
-      return next(null, offering);
-    } catch (error) {
-      error = new VError(error, 'error parsing offering "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/disciplines/' + discipline + '/offerings/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
 
-exports.catalogs = function (page, next) {
+exports.catalogs = function (next) {
   'use strict';
 
-  return httpRequest(uri + '/catalogs?page=' + page, function (error, res, body) {
-    var catalogs;
-    if (error) {
-      error = new VError(error, 'error requesting catalogs');
-      return next(error);
-    }
-    try {
-      catalogs = JSON.parse(body);
-      return next(null, catalogs);
-    } catch (error) {
-      error = new VError(error, 'error parsing catalogs to json');
-      return next(error);
-    }
-  });
+  return downloadPages('/catalogs', next);
 };
 
 exports.catalog = function (id, next) {
   'use strict';
 
-  return httpRequest(uri + '/catalogs/' + id, function (error, res, body) {
-    var catalog;
-    if (error) {
-      error = new VError(error, 'error requesting catalog "$s"', id);
-      return next(error);
-    }
-    try {
-      catalog = JSON.parse(body);
-      return next(null, catalog);
-    } catch (error) {
-      error = new VError(error, 'error parsing catalog "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/catalogs/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
 
-exports.modalities = function (catalog, page, next) {
+exports.modalities = function (catalog, next) {
   'use strict';
 
-  return httpRequest(uri + '/catalogs/' + catalog + '/modalities?page=' + page, function (error, res, body) {
-    var modalities;
-    if (error) {
-      error = new VError(error, 'error requesting modalities');
-      return next(error);
-    }
-    try {
-      modalities = JSON.parse(body);
-      return next(null, modalities);
-    } catch (error) {
-      error = new VError(error, 'error parsing modalities to json');
-      return next(error);
-    }
-  });
+  return downloadPages('/catalogs/' + catalog + '/modalities', next);
 };
 
 exports.modality = function (catalog, id, next) {
   'use strict';
 
-  return httpRequest(uri + '/catalogs/' + catalog + '/modalities/' + id, function (error, res, body) {
-    var modality;
-    if (error) {
-      error = new VError(error, 'error requesting modality "$s"', id);
-      return next(error);
-    }
-    try {
-      modality = JSON.parse(body);
-      return next(null, modality);
-    } catch (error) {
-      error = new VError(error, 'error parsing modality "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/catalogs/' + catalog + '/modalities/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
 
-exports.blocks = function (catalog, modality, page, next) {
+exports.blocks = function (catalog, modality, next) {
   'use strict';
 
-  return httpRequest(uri + '/catalogs/' + catalog + '/modalities/' + modality + '/blocks?page=' + page, function (error, res, body) {
-    var blocks;
-    if (error) {
-      error = new VError(error, 'error requesting blocks');
-      return next(error);
-    }
-    try {
-      blocks = JSON.parse(body);
-      return next(null, blocks);
-    } catch (error) {
-      error = new VError(error, 'error parsing blocks to json');
-      return next(error);
-    }
-  });
+  return downloadPages('/catalogs/' + catalog + '/modalities/' + modality + '/blocks', next);
 };
 
 exports.block = function (catalog, modality, id, next) {
   'use strict';
 
-  return httpRequest(uri + '/catalogs/' + catalog + '/modalities/' + modality + '/blocks/' + id, function (error, res, body) {
-    var block;
-    if (error) {
-      error = new VError(error, 'error requesting block "$s"', id);
-      return next(error);
-    }
-    try {
-      block = JSON.parse(body);
-      return next(null, block);
-    } catch (error) {
-      error = new VError(error, 'error parsing block "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/catalogs/' + catalog + '/modalities/' + modality + '/blocks/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
